70bd87ca105dde41d2559d0c19b2a3947bbeba0e
Lucas Muniz <lkz.lmuniz@gmail.com>
2021-07-17 20:44:26 -0300

    adds timber log

--- app/src/main/java/br/com/muniz/activityrecognition/MyApp.kt
+++ app/src/main/java/br/com/muniz/activityrecognition/MyApp.kt
@@ -2,7 +2,15 @@ package br.com.muniz.activityrecognition
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
