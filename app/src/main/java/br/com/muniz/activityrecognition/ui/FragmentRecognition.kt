9d3c88e7eba07c93e838726ef87a40127c058bbd
Lucas Muniz <lkz.lmuniz@gmail.com>
2021-08-01 17:54:57 -0300

    layout adjustments

--- app/src/main/java/br/com/muniz/activityrecognition/ui/FragmentRecognition.kt
+++ app/src/main/java/br/com/muniz/activityrecognition/ui/FragmentRecognition.kt
@@ -2,18 +2,28 @@ package br.com.muniz.activityrecognition.ui
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import br.com.muniz.activityrecognition.R
+import br.com.muniz.activityrecognition.Util
+import br.com.muniz.activityrecognition.data.WekaDataIntensityLying
+import br.com.muniz.activityrecognition.data.WekaDataIntensitySit
+import br.com.muniz.activityrecognition.data.WekaDataIntensityWalk
+import br.com.muniz.activityrecognition.databinding.FragmentRecognitionBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -22,6 +32,7 @@ class FragmentRecognition : Fragment(), SensorEventListener {
 
     private var sensor: Sensor? = null
     private var sensorManager: SensorManager? = null
+    private lateinit var binding: FragmentRecognitionBinding
 
     private val recognitionViewModel: FragmentRecognitionViewModel by viewModels()
 
@@ -35,18 +46,72 @@ class FragmentRecognition : Fragment(), SensorEventListener {
         unregisterSensor()
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recognition, container, false)
+
         recognitionViewModel.finalMovement.observe(viewLifecycleOwner, {
             it?.let {
-               val a = requireView().findViewById(R.id.tv_activity) as TextView
-                a.text = "${it.movement} / ${it.intensity}"
+                var color: Int
+                var image: Int
+                var finalActivity = ""
+                when (it.movement) {
+                    in Util.WALK_RANGE -> {
+                        image = R.drawable.ic_walk
+                        finalActivity = getString(R.string.walk)
+                    }
+                    in Util.SIT_RANGE -> {
+                        image = R.drawable.ic_sit
+                        finalActivity = getString(R.string.sit)
+                    }
+                    in Util.LYING_RANGE -> {
+                        image = R.drawable.ic_lyingdown
+                        finalActivity = getString(R.string.lying)
+                    }
+                    else -> {
+                        image = R.drawable.ic_walk
+                        finalActivity = ""
+                    }
+                }
+
+                finalActivity += " "
+
+                when (it.intensity) {
+                    0 -> {
+                        color = R.color.soft
+                        finalActivity += getString(R.string.soft)
+                    }
+                    1 -> {
+                        color = R.color.medium
+                        finalActivity += getString(R.string.moderate)
+                    }
+                    2 -> {
+                        color = R.color.high
+                        finalActivity += getString(R.string.high)
+                    }
+                    else -> {
+                        color = R.color.soft
+                        finalActivity = ""
+                    }
+                }
+
+                val drawable = ContextCompat.getDrawable(
+                    requireContext(),
+                    image
+                )
+                drawable?.setTint(ContextCompat.getColor(requireContext(), color))
+                binding.tvActivity.text = finalActivity
+                binding.tvActivity.setTextColor(ContextCompat.getColor(requireContext(), color))
+                binding.imgFinalActivity.setImageDrawable(drawable)
+                binding.imgFinalActivity.visibility = View.VISIBLE
             }
         })
-        return inflater.inflate(R.layout.fragment_recognition, container, false)
+        return binding.root
     }
 
     override fun onSensorChanged(p0: SensorEvent?) {
